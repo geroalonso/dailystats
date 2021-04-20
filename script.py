7dac53defd450e78f660acfdc1d414da74063cc2
Geronimo Alonso <geronimoalonso@icloud.com>
2021-04-20 17:16:21 -0500

    continua

--- script.py
+++ script.py
@@ -14,9 +14,6 @@ sched = BlockingScheduler()
 
 @sched.scheduled_job('interval', minutes=5)
 def timed_job():
-    print('This job is run every five minutes.')
-
-	#TSHEETS
 	url = "https://rest.tsheets.com/api/v1/timesheets"
 
 	querystring = {
